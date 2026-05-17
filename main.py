8ac44a0cf4d8fdf07ff5debcc8da53b8ef2e847e
TallJakub <talljakubowski@gmail.com>
2026-05-17 13:40:32 +0200

    Peashooter teraz strzela pociskami co 0.75 sekundy

--- main.py
+++ main.py
@@ -34,8 +34,8 @@ class uczenZSK:
 
 class peashooter:
     def __init__(self):
-        self.x_cord = 100
-        self.y_cord = 100
+        self.x_cord = 200
+        self.y_cord = 60
         self.image = pygame.image.load(os.path.join(scriptDir, 'assets', 'peashooterZsk.png'))
         
         self.width = self.image.get_width()
@@ -45,12 +45,40 @@ class peashooter:
     def draw(self):
         window.blit(self.image,(self.x_cord, self.y_cord))
 
+class peaBall:
+    def __init__(self, peashooter):
+        self.peashooter = peashooter
+        self.x_cord = peashooter.x_cord + peashooter.width - 20
+        self.y_cord = peashooter.y_cord + 25
+        self.image = pygame.image.load(os.path.join(scriptDir, 'assets', 'peaBall.png'))
+        
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
+        
+        self.hitbox = pygame.Rect(self.x_cord, self.y_cord, self.width, self.height)
+        
+    def draw(self):
+        window.blit(self.image, (self.x_cord, self.y_cord))
+    
+    def move(self):
+        self.x_cord += 8
+    
+    def isOffscreen(self):
+        return self.x_cord > 1450
+
+
 def main():
     run = True
     player = uczenZSK()
     defender = peashooter()
+    peaballs = []
+    
     clock = pygame.time.Clock()
+    
     background = pygame.image.load(os.path.join(scriptDir, 'assets', 'ogrod.jpg'))
+    
+    shoot_cooldown = 0
+    shoot_rate = 45
     
     while run:
         clock.tick(60) 
@@ -64,9 +92,27 @@ def main():
         
         player.move(keys)
         
+        if shoot_cooldown > 0:
+            shoot_cooldown -= 1
+        
+        if shoot_cooldown == 0:
+            peaballs.append(peaBall(defender))
+            shoot_cooldown = shoot_rate
+        
+        if clock == 1:
+            clock = 0
+            peaballs.append(peaBall(defender))
+        
+        for peaball in peaballs[:]:
+            peaball.move()
+            if peaball.isOffscreen():
+                peaballs.remove(peaball)
+        
         window.blit(background,(0,0))
-        player.draw()
+        for peaball in peaballs:
+            peaball.draw()
         defender.draw()
+        player.draw()
         pygame.display.update()
 
 if __name__== "__main__":
